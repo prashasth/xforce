14d80f5472717fac64e640558fe6418e8110a188
Prashasth Baliga <prashasth1994@gmail.com>
2019-09-06 23:14:38 +0800

    Create iprep_conf.py

---
+++ iprep_conf.py
@@ -0,0 +1,18 @@
+# Basic configuration file
+
+#Alienvault OTX 
+#via https://otx.alienvault.com/api/
+otx_authkey = ""
+
+#XForce exchange i
+# via https://exchange.xforce.ibmcloud.com/new
+# Settings -> API key
+xfex_cred = "KEY":"PASSWORD"
+
+#Virustotal 
+#via https://www.virustotal.com/
+vt_key = ""
+
+#Google APIKey
+# via https://console.developers.google.com/apis/api/safebrowsing.googleapis.com/
+go_key = ""
