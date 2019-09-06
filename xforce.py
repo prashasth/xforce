a63ec1ce87572f0f86ce577f5d443fc37d6f564f
Prashasth Baliga <prashasth1994@gmail.com>
2019-09-06 23:13:56 +0800

    Update xforce.py

--- xforce.py
+++ xforce.py
@@ -7,11 +7,33 @@ import re
 from datetime import datetime, timedelta
 import sys
 import base64
-headers = {'Accept': 'application/json', 'Authorization': 'Basic YzY2YjViZmUtYzNkZC00MjIxLTg3ZDMtYTUwYTc5MWNlZGUxOmZmZTMzODkwLWY4NTktNDA0Zi1iODc2LThmNGU5ODFjYTJiOQ== ', 'User-Agent': 'Mozilla 5.0'}
+
+BASEurl = "https://api.xforce.ibmcloud.com/"
+
+sys.path.append('./')
+
+try:
+    import iprep_conf as IC
+    assert IC.xfex_cred != "key:secret"
+except:
+    print "Please create a config file which contains a valid set of API credentials."
+    sys.exit(1)
+# End try/except block
+
+authstring = base64.encodestring(IC.xfex_cred).replace('\n','')
+yesterday = datetime.now() - timedelta(days=1)
+YEST = yesterday.strftime('20%y-%m-%dT00:00:00Z')
+
+headers = {"Authorization": "Basic %s " % authstring,
+    "Accept": "application/json",
+    'User-Agent': 'Mozilla 5.0'}
+#headers = {'Accept': 'application/json', 'Authorization': 'Basic YzY2YjViZmUtYzNkZC00MjIxLTg3ZDMtYTUwYTc5MWNlZGUxOmZmZTMzODkwLWY4NTktNDA0Zi1iODc2LThmNGU5ODFjYTJiOQ== ', 'User-Agent': 'Mozilla 5.0'}
 ip_list = [line.rstrip('\n') for line in open('filename.txt')]
 for ip in ip_list:
-  url_req = "https://api.xforce.ibmcloud.com/ipr/"+ip
-  req = urllib2.Request(url_req, None, headers)
+  furl = BASEurl + "ipr/%s" % ip
+  furl2 = BASEurl + "ipr/malware/%s" % ip
+  #url_req = "https://api.xforce.ibmcloud.com/ipr/"+ip
+  req = urllib2.Request(furl, None, headers)
   response = urllib2.urlopen(req).read()
   response_json = json.loads(response)
   print "THE RESULTS FOR THE IP "+ip+ " :"
